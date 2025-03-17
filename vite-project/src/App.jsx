2086c23a7138cfdff570d2a4735e21164be4efc4
Rajalakshmi <sec23cj001@sairamtap.edu.in>
2025-03-17 20:22:10 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='home'>Welcome to our group project</h1>
+    <h2>This is our home page.</h2> 
+        
+    </>
+  )
+}
+
+export default App
